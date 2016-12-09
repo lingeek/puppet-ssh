d64ee7983f4928c4235c0904f66aaec89c621a37
lingeek <raul@deva-alpine-adventure.org>
2016-12-09 02:12:37 -0500

    Commit on master branch

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 #This is the class for manageing SSH
 
 class ssh (
+#Master Branch
 #This value is in SuSe.yaml/eb3
 $sshd_package             = hiera('sshd_package'),
 $service_name             = hiera('sshd::service'),
